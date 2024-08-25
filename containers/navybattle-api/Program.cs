5f8500f7ca401bdf0fd23e82fe5ae5b92a633fbc
Don <sleepyparadox@gmail.com>
2024-08-25 19:08:21 +1000

    added basic "hello-world" index page to navybattle-api

--- containers/navybattle-api/Program.cs
+++ containers/navybattle-api/Program.cs
@@ -1,11 +1,29 @@
+using System.Text.Json.Serialization;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers().AddJsonOptions(options =>
+{
+    options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+}); ;
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+              .AllowAnyMethod()
+              .AllowAnyHeader()
+              .WithExposedHeaders("ETag");
+    });
+});
 
 var app = builder.Build();
 
@@ -16,10 +34,19 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors("AllowAll");
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+// Serve ui files
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+// Fallback to ui files if route not found
+app.MapFallbackToFile("index.html");
 
 app.Run();
